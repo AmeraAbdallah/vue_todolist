d7343fdae3922b114a184a7f1a26ce0012557e83
Amera Ayad <amera.ayad.it@gmail.com>
2020-02-29 00:07:02 +0200

    remove extra code and add todos data

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,41 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+  },
+  data: () => {
+    return {
+      todos: [{
+        id: '1',
+      text: 'todo one',
+      completed: false
+    }, {
+      id: '2',
+      text: 'todo two',
+      completed: true
+    }]
+    }
   }
 }
 </script>
 
 <style>
-#app {
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+body {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
